ff31d3eff1bb80c61b06272e188ffed0ff5f60d8
akash-kalk <akash@kalksolutions.com>
2026-05-25 12:55:37 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/Kalk-Solutions/",
   plugins: [TanStackRouterVite({ target: "react", autoCodeSplitting: true }), react(), tailwindcss(), tsconfigPaths()],
 });
